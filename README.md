787276eb1a3acf164c972a9a06c757f2f6d2ba68
vaughanlove <vaughanlove1@gmail.com>
2023-11-10 17:56:50 -0500

    read pdf

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # gc-api
+
+## run locally
+
+ uvicorn app.main:app --host 0.0.0.0 --port 5000
+
+ ## run docker container for hosting
+
+ docker compose up
+
+ ## upload pdf example
+
+  curl -X POST -F "file=@vaughan_love_resume.pdf" http://localhost:5000/uploadfile/
--- app/main.py
+++ app/main.py
@@ -1,15 +1,28 @@
-from typing import Union
+from typing import Union, Annotated
+import os
+import pypdf
 
-from fastapi import FastAPI
+from llmsherpa.readers import LayoutPDFReader
+
+from fastapi import FastAPI, File, UploadFile
+
+# llm sherpa for reading pdfs
+llmsherpa_api_url = "https://readers.llmsherpa.com/api/document/developer/parseDocument?renderFormat=all"
+pdf_reader = LayoutPDFReader(llmsherpa_api_url)
 
 app = FastAPI()
-
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+@app.post("/read_pdf/")
+async def read_pdf(file: Annotated[bytes, File()]):
+    # the path_or_url is fake, ignored when contents is set.
+    content = pdf_reader.read_pdf(path_or_url="https://someexapmple.com/myfile.pdf", contents=file)
+    docs = []
+    for section in content.sections():
+        docs.append(section.to_text(include_children=True, recurse=True))
+    return {"content": docs }
+    
