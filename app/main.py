2ade4bb39f6c06dc50a509d78b3f87aff2b6e9e0
Alex <alexanderv1414@gmail.com>
2023-11-12 12:47:02 -0700

    Update main.py

--- app/main.py
+++ app/main.py
@@ -82,7 +82,7 @@ async def extract_url(payload: URLPayload):
 @app.post("/read_pdf/")
 async def read_pdf(file: Annotated[bytes, File()]):
     # the path_or_url is fake, ignored when contents is set.
-    content = pdf_reader.read_pdf(path_or_url="", contents=file)
+    content = pdf_reader.read_pdf(path_or_url="https://someexapmple.com/myfile.pdf", contents=file)
     docs = []
     for section in content.sections():
         docs.append(section.to_text(include_children=True, recurse=True))
