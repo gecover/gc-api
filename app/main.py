5b37c081fde183d5f00430e7295044b52fff3d57
vaughanlove <vaughanlove1@gmail.com>
2023-11-28 21:07:04 -0700

    import time

--- app/main.py
+++ app/main.py
@@ -14,6 +14,7 @@ from dotenv import load_dotenv
 from pydantic import BaseModel
 from app.scrape.ScrapeClient import ScrapingClient
 from openai import OpenAI
+import time
 
 
 class URLPayload(BaseModel):
